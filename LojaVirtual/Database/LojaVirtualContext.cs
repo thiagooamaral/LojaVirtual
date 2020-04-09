532d9e9d2e6e184345a46a260b80174b4331466d
Thiago Amaral <tfmamaral@outlook.com>
2020-04-09 17:41:13 -0300

    newsletter development

---
+++ LojaVirtual/Database/LojaVirtualContext.cs
@@ -0,0 +1,19 @@
+﻿using LojaVirtual.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LojaVirtual.Database
+{
+    public class LojaVirtualContext : DbContext
+    {
+        //EF Core: ORM
+        //ORM: Biblioteca para mapear objetos para o Banco de Dados relacional
+
+        public LojaVirtualContext(DbContextOptions<LojaVirtualContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<NewsletterEmail> NewsletterEmails { get; set; }
+    }
+}
--- LojaVirtual/Startup.cs
+++ LojaVirtual/Startup.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using LojaVirtual.Database;
 
 namespace LojaVirtual
 {
@@ -33,6 +31,9 @@ namespace LojaVirtual
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            string connection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LojaVirtual;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            services.AddDbContext<LojaVirtualContext>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
